da6dd550a35b31022a2c7e835460297ddac029e9
RodionSverdlovAl <rsverdlovalexandrovich@gmail.com>
2022-06-30 03:25:20 +0300

    add put and delete request

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -7,16 +7,28 @@ const PostContainer:React.FC = () =>{
     const [limit, setLimit] = useState(100)
     const {data:posts, isLoading} = postAPI.useFetchAllPostsQuery(limit)
     const [createPost, {}] = postAPI.useCreatePostMutation()
+    const [deletePost, {}] = postAPI.useDeletePostMutation()
+    const [updatePost, {}] = postAPI.useUpdatePostMutation()
+
     const handleCreate = async() =>{
         const title = prompt('Enter post title');
         const body = prompt('Enter post body');
         await createPost({title: title, body: body}as IPost)
     }
+
+    const handleUpdate = (post: IPost) =>{
+        updatePost(post)
+    }
+
+    const handleDelete = (post:IPost) =>{
+        deletePost(post);
+    }
+
     return(
         <div>
             <button onClick={handleCreate}>Add new Post</button>
             {isLoading && <h1>Loading...</h1>}
-            {posts?.map(post=><PostItem key ={post.id} post = {post}/>)}
+            {posts?.map(post=><PostItem update= {handleUpdate} remove={handleDelete} key ={post.id} post = {post}/>)}
         </div>
     )
 }
--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -2,14 +2,24 @@ import { IPost } from "../models/IPost"
 
 interface PostItemProps{
     post: IPost;
+    remove: (post: IPost)=> void;
+    update: (post: IPost)=> void;
 }
 
-const PostItem:React.FC<PostItemProps> =({post}) =>{
+const PostItem:React.FC<PostItemProps> =({post, remove, update}) =>{
+
+    const handleUpdate=()=>{
+        const title = prompt('Enter new title') || "";
+        update({...post, title})
+    }
+    
+
     return(
         <div>
             <h3>{post.id} {post.title}</h3>
             <p>{post.body}</p>
-            <button>Delete</button>
+            <button onClick={()=>remove(post)}>Delete</button>
+            <button onClick={handleUpdate}>Update</button>
 
         </div>
     )
